60faf0801ccbf8c934f71da3ce626631cb2a40b0
Monique St. Laurent and Susha Dore <monique@>
2017-06-01 10:36:27 -0700

    add delete and update functionality

--- app/routes/entry.js
+++ app/routes/entry.js
@@ -5,6 +5,15 @@ export default Ember.Route.extend({
     return this.store.findRecord('entry', params.entry_id);
   },
   actions: {
+    update(entry, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          entry.set(key,params[key]);
+        }
+      });
+      entry.save();
+      this.transitionTo("entry");
+    },
     destroyEntry(entry) {
       entry.destroyRecord();
       this.transitionTo("index");
